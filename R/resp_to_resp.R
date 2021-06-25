270a61f0818e7eab416e00da282610c54f867af1
jburnett <jburnett@usgs.gov>
2021-06-25 12:11:45 -0600

    Update resp_to_resp.R

--- R/resp_to_resp.R
+++ R/resp_to_resp.R
@@ -20,10 +20,31 @@ resp_to_resp <-
                               aff_to_resp$ThoughtIdA
     ))
 
-if(!all(question_to_resp$ThoughtIdA %in% questions$Id)) warning("see line 4 make_response_tag_link_tables.R")
-
 #remove these from links
-links <- setdiff(links, question_to_resp)
+links <- setdiff(links, resp_to_resp)
+
+
+
+# Check to maek sure we have all links covered. ---------------------------
+links.test <- links %>%
+    filter(!ThoughtIdA %in% unique(nodes_orig$TypeId)) %>%
+    distinct(ThoughtIdA, ThoughtIdB, Name.Parent, Name.Child)
+
+links.test <- links.test %>%
+    filter(!ThoughtIdA %in% c(jlb_to_para$ThoughtIdA,
+                                                         jlb_to_resp$ThoughtIdA,
+                                                         para_to_resp$ThoughtIdA,
+                                                         euts_to_jlb$TypeId.Parent,
+                                                         euts_to_para$TypeId.Parent,
+                                                         person_to_resp$ThoughtIdA,
+                                                         question_to_resp$ThoughtIdA,
+                                                         aff_to_resp$ThoughtIdA
+    ))
+
+
+## the remaining links are also resp resp to resp..
+
+links.test
 
 
 # Save objs to export -----------------------------------------------------
