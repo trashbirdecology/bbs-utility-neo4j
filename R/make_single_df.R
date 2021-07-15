b3ce9339c41215b8d1a8b13250fd59b981604286
jburnett <jburnett@usgs.gov>
2021-07-15 14:48:19 -0600

    Still need to do more work to get the single df correct...
    notes placed in the R.

--- R/make_single_df.R
+++ R/make_single_df.R
@@ -1,14 +1,32 @@
+## I need to join these things together in order of most broad to least
+
 # A bunch of joins--------------------------------------------------------df <-
-df <- aff_to_resp %>%
-    select(AffliationName, RespName, AffiliationId, RespId) %>%
-    full_join(person_to_resp %>% select(PersonName, RespName, PersonId, RespId)) %>%
+df <- euts_to_jlb %>% select(JlbName, JlbId, EutsName, EutsId) %>%
+    full_join(euts_to_para %>% select(ParaName, ParaId, EutsName, EutsId)) %>%
     full_join(para_to_resp %>% select(ParaName, RespName, RespId, ParaId)) %>%
+    full_join(jlb_to_resp %>% select(JlbName, RespName, JlbId, RespId))
+
+
+
+    full_join(aff_to_resp) %>% select(AffliationName, RespName, AffiliationId, RespId) %>%
+    full_join(person_to_resp %>% select(PersonName, RespName, PersonId, RespId)) %>%
     full_join(question_to_resp %>% select(QuestionName, QuestionId, RespName, RespId)) %>%
     full_join(resp_to_para %>% select(RespName, ParaName, ParaId, RespId)) %>%
-    full_join(resp_to_resp %>% select(RespNameParent, RespNameChild, RespIdChild, RespIdParent), by=c("RespName"="RespNameParent", "RespId"="RespIdParent")) %>%
+    full_join(resp_to_resp %>% select(RespNameParent, RespNameChild, RespIdChild, RespIdParent),
+              by=c("RespName"="RespNameParent", "RespId"="RespIdParent")) %>%
     full_join(resp_to_jlb %>% select(RespId, JlbId, RespName, JlbName)) %>%
-    full_join(euts_to_para %>% select(ParaName, ParaId, EutsName, EutsId)) %>%
-    full_join(euts_to_jlb %>% select(JlbName, JlbId, EutsName, EutsId))
+
+
+# Reorder names for my sanity ---------------------------------------------
+df <- df %>% relocate(QuestionName, EutsName, JlbName, ParaName, RespName, RespNameChild, AffliationName, PersonName)
+
+
+# Toying around with conditional relationships ---------------------------------------------
+
+temp <- df %>% filter(!is.na(EutsName) & !is.na(JlbName))
+temp <- setdiff(df, temp)
+
+
 
 
 # Create indices for rules ---------------------------------------------------------
@@ -24,9 +42,14 @@ unique(resp_to_para$ParaId)
 e <- c(unique(euts_to_jlb$EutsId), unique(euts_to_para$EutsId))
 
 
+# HIDDEN RELATIONSHIPS ------------------------
+aff_to_resp %>% distinct(RespId)
+aff_to_resp %>% distinct(RespId)
+
+
 
 # Save objs to export -----------------------------------------------------
-export <- c(paste(export), "data_all")
+export <- c(paste(export), "df")
 
 # remove all else from mem
 rm(list=setdiff(ls(), export))
